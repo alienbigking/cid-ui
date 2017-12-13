660b2ba3f0606a0a45b3a844ee51ac84641d3c46
twmay006 <twmay006@163.com>
2017-12-13 11:54:30 +0800

    新增监区文件夹，监区租户模块组件已完成

--- src/application/common/service/menu-service.js
+++ src/application/common/service/menu-service.js
@@ -3,50 +3,57 @@
 export default [
     {
         index: '1',
-        name: '刑罚执行',
+        name: '监区',
         collapse: true,
         children: [
             {
                 index: '1-1',
-                name: '收监管理',
-                path: '/tenant/detail',
+                name: '查询监区',
+                path: '/prison-area/list',
                 collapse: true
             },
             {
                 index: '1-2',
-                name: '减刑假释',
-                collapse: true,
-                children: [
-                    {
-                        index: '1-2-1',
-                        name: '刑罚变动',
-                        path: 'login',
-                        collapse: false
-                    },
-                    {
-                        index: '1-2-2',
-                        name: '监区办理',
-                        path: 'login',
-                        collapse: false
-                    },
-                    {
-                        index: '1-2-3',
-                        name: '其他处理',
-                        path: 'login',
-                        collapse: false
-                    }
-                ]
+                name: '监区明细',
+                path: '/prison-area/detail',
+                collapse: true
             },
+
+            // {
+            //     index: '1-3',
+            //     name: '减刑假释',
+            //     collapse: true,
+            //     children: [
+            //         {
+            //             index: '1-3-1',
+            //             name: '刑罚变动',
+            //             path: 'login',
+            //             collapse: false
+            //         },
+            //         {
+            //             index: '1-3-2',
+            //             name: '监区办理',
+            //             path: 'login',
+            //             collapse: false
+            //         },
+            //         {
+            //             index: '1-3-3',
+            //             name: '其他处理',
+            //             path: 'login',
+            //             collapse: false
+            //         }
+            //     ]
+            // },
             {
-                index: '1-3',
-                name: '监外执行',
-                path: '/tenant/list',
+                index: '1-4',
+                name: '新增监区',
+                path: '/prison-area/new',
                 collapse: true
             },
             {
-                index: '1-4',
-                name: '出监管理',
-                path: '/tenant/new',
+                index: '3-5',
+                name: '修改监区',
+                path: '/prison-area/edit',
                 collapse: true
             }
         ]
@@ -103,50 +110,31 @@ export default [
     },
     {
         index: '3',
-        name: '档案管理',
+        name: '租户',
         collapse: true,
         children: [
             {
                 index: '3-1',
-                name: '收监管理',
-                path: 'dashboard',
+                name: '租户列表',
+                path: '/tenant/list',
                 collapse: true
             },
             {
-                index: '1-2',
-                name: '减刑假释',
-                collapse: true,
-                children: [
-                    {
-                        index: '1-2-1',
-                        name: '刑罚变动',
-                        path: 'login',
-                        collapse: false
-                    },
-                    {
-                        index: '1-2-2',
-                        name: '监区办理',
-                        path: 'login',
-                        collapse: false
-                    },
-                    {
-                        index: '1-2-3',
-                        name: '其他处理',
-                        path: 'login',
-                        collapse: false
-                    }
-                ]
-            },
-            {
-                index: '1-3',
-                name: '监外执行',
-                path: 'dashboard',
+                index: '3-2',
+                name: '租户明细',
+                path: '/tenant/detail',
                 collapse: true
             },
             {
-                index: '1-4',
-                name: '出监管理',
-                path: 'dashboard',
+                index: '3-4',
+                name: '新增租户',
+                path: '/tenant/new',
+                collapse: true
+            },
+            {
+                index: '3-5',
+                name: '修改租户',
+                path: '/tenant/edit',
                 collapse: true
             }
         ]
---
+++ src/application/prison-area/components/index.js
@@ -0,0 +1,4 @@
+export { default as areaList } from './area-list';
+export { default as areaNew } from './area-new';
+export { default as areaEdit } from './area-edit';
+export { default as areaDetail } from './area-detail';
