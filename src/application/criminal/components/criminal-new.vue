130b443bc10faf69cfb2c9d30459d01bf6254ad2
twmay006 <twmay006@163.com>
2017-12-25 11:30:11 +0800

    修改

--- src/application/criminal/components/criminal-new.vue
+++ src/application/criminal/components/criminal-new.vue
@@ -209,7 +209,7 @@
 
 <script>
 import { mapState, mapActions } from "vuex";
-import {default as regionLookupService} from '@/application/region/service/region-lookup-service';
+import {default as regionLookupService} from '@/application/common/service/region-lookup-service';
 import _ from "lodash";
 export default {
   data() {
