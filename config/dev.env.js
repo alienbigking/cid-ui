3f7cb886c3aaa40aaf3301e99a622b85252f7116
Julius Metasequoia <julius.metasequoia@gmail.com>
2017-12-25 17:59:06 +0800

    实现多环境发布配置。

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,6 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  CID_BASE_URL:'"http://10.10.10.140:8080/cid-development/"'
 })
