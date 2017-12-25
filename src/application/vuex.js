b0394ded5ab52ea29e1ea150d90f9cf86b27fb76
AbrahamJay <abrahamjay@163.com>
2017-12-25 11:29:50 +0800

    合并代码

--- src/application/vuex.js
+++ src/application/vuex.js
@@ -1,5 +1,4 @@
 import { vuex as common } from './common';
-import { vuex as region } from './region';
 import { vuex as login } from './login';
 import { vuex as me } from './me';
 import { vuex as tenant } from './tenant';
@@ -8,4 +7,4 @@ import { vuex as prisonHouse } from './prison-house';
 import { vuex as criminal } from './criminal';
 import { vuex as role } from './role';
 
-export default { common, region, login, me, tenant, prisonArea, prisonHouse, criminal, role };
+export default { common, login, me, tenant, prisonArea, prisonHouse, criminal, role };
