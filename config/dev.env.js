00cf58b8ddee4a787adc8b1a3dd16d7b408caac7
Julius Metasequoia <julius.metasequoia@gmail.com>
2018-01-12 18:16:30 +0800

    重构代码。

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,4 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   CID_BASE_URL:'"http://10.10.10.140:8080/cid-development/"'
-  // CID_BASE_URL:'"http://10.10.10.107:8080/"'
 })
