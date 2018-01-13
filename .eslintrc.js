4468eeea8c310fc6624edd972884a7172f65dbe9
Julius Metasequoia <julius.metasequoia@gmail.com>
2018-01-13 12:36:12 +0800

    升级库到最新版本。

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,24 @@
 module.exports = {
     root: true,
-    parser: 'babel-eslint',
+    parser: "babel-eslint",
     parserOptions: {
-        sourceType: 'module'
+        sourceType: "module"
     },
     env: {
-        browser: true,
+        browser: true
     },
 
-    extends: 'standard',
+    extends: "standard",
     // required to lint *.vue files
-    plugins: [
-        'html'
-    ],
+    plugins: ["html"],
     rules: {
         // allow async-await
-        'generator-star-spacing': 'off',
+        "generator-star-spacing": "off",
         // allow debugger during development
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        "semi": [2, "always"],
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+        semi: [2, "always"],
         "space-before-function-paren": ["error", "never"],
-        "indent": "off",
-        "quotes": "off"
+        indent: "off",
+        quotes: "off"
     }
-}
+};
--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -48,10 +48,11 @@ const webpackConfig = merge(baseWebpackConfig, {
     // extract css into its own file
     new ExtractTextPlugin({
       filename: utils.assetsPath('css/[name].[contenthash].css'),
-      // set the following option to `true` if you want to extract CSS from
-      // codesplit chunks into this main css file as well.
-      // This will result in *all* of your app's CSS being loaded upfront.
-      allChunks: false,
+      // Setting the following option to `false` will not extract CSS from codesplit chunks.
+      // Their CSS will instead be inserted dynamically with style-loader when the codesplit chunk has been loaded by webpack.
+      // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`, 
+      // increasing file size: https://github.com/vuejs-templates/webpack/issues/1110
+      allChunks: true,
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
@@ -79,14 +80,14 @@ const webpackConfig = merge(baseWebpackConfig, {
       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
       chunksSortMode: 'dependency'
     }),
-    // keep module.id stable when vender modules does not change
+    // keep module.id stable when vendor modules does not change
     new webpack.HashedModuleIdsPlugin(),
     // enable scope hoisting
     new webpack.optimize.ModuleConcatenationPlugin(),
     // split vendor js into its own file
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
-      minChunks(module) {
+      minChunks (module) {
         // any required modules inside node_modules are extracted to vendor
         return (
           module.resource &&
