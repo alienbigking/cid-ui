dd9334690dfa7c2316e42dfe086ba79ac9624ab7
unknown <legolasedwin@github.com>
2018-01-19 17:32:25 +0800

    表单文本内容靠近LABEL

--- src/application/criminal/components/criminal-detail.vue
+++ src/application/criminal/components/criminal-detail.vue
@@ -68,7 +68,7 @@
                       <label>政治面貌：</label><span>{{criminal.politicalStatusName}}</span>
                   </el-col>
                   <el-col :span="6">
-                      <label class="label120">是否参加党派团体：</label><span class="span120">{{criminal.politicalParty?"是":"否"}}</span>
+                      <label>是否参加党派团体：</label><span class="span120">{{criminal.politicalParty?"是":"否"}}</span>
                   </el-col>
                   <el-col :span="6">
                       <label>文化程度：</label><span>{{criminal.educationDegreeName}}</span>
@@ -282,7 +282,7 @@ export default {
   }
   .card-body{
     label{
-      width: 80px;
+      width: auto;
     }
     span{ width: calc(100% - 100px); }
     .label120{
