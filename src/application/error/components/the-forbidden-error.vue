17e7271eccac9d9745048744eb6e499c7a4cc4be
twmay006 <twmay006@163.com>
2018-01-22 14:23:05 +0800

    error页面样式调整

--- src/application/error/components/the-forbidden-error.vue
+++ src/application/error/components/the-forbidden-error.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="error">
+  <el-container class="flex-column">
     <the-error-header></the-error-header>
-    <div class="error-box">
+    <el-main class="error-box">
       <div class="error-left">
         <div class="error-number"></div>
         <p class="error-reason">对不起，您无权访问该页面！</p>
-        <p><el-button type="text" class="error-button" @click="onHome">回到主页 >></el-button></p>
+        <el-button type="text" class="error-button" @click="onHome">回到主页 >></el-button>
       </div>
-      <div class="error-right">
-        <div class="error-image"></div>
-      </div>
-    </div>
+      <div class="error-right"></div>
+    </el-main>
     <the-error-footer></the-error-footer>
-  </div>
+  </el-container>
 </template>
 
 <script>
@@ -32,50 +30,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.error {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: #f5f5f5;
-  .error-box{
-    display: flex;
-    flex-direction: row;
-    transform: translateY(50%);
-    width: 650px;
-    box-sizing: content-box;
-    .error-left{
-      width:300px;
-      margin-top: 50px;
-      margin-left: 55px;
-      .error-number{
-        width:151px;
-        height: 74px;
-        background: url(../../../assets/images/not-found-resource-number.png) 0 0 no-repeat;
-        background-size: 151px 74px;
-      }
-      .error-reason{
-        font-size:18px;
-        margin-top:33px;
-        margin-bottom: 12px;
-      }
-      .error-button{
-        color:red;
-      }
-    }
-    .error-right{ 
-      width:300px;
-      height: 283px;
-      margin-left:50px;
-      .error-image{
-        width:281px;
-        height: 283px;
-        background: url(../../../assets/images/not-found-resource-icon.png) 0 0 no-repeat;
-        background-size: 281px 283px;
-      }
-    }
-  }
+@import "./error";
+.error-number{
+  background: url(../../../assets/images/not-found-resource-number.png) 0 0 no-repeat;
+  background-size: 151px 74px;
+}
+
+.error-right{
+  background: url(../../../assets/images/not-found-resource-icon.png) 0 0 no-repeat;
+  background-size: 281px 283px;
 }
 </style>
-
