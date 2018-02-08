08d90a9cd479940c790bc9e7f0cbaa93aa817db1
AbrahamJay <abrahamjay@163.com>
2018-02-08 14:53:19 +0800

    修改罪犯体貌特征bug

--- src/application/me/vuex/mutations.js
+++ src/application/me/vuex/mutations.js
@@ -5,11 +5,10 @@ import _ from "lodash";
 export default {
   [types.SET_MY_PROFILE](state, myProfile) {
     state.myProfile = myProfile;
-  },
-  [types.SET_PROFILE_STORAGE](state, myProfile) {
     profileStorage.setMyProfile(myProfile);
   },
   [types.UPDATE_MY_PROFILE](state, myProfile) {
     state.myProfile = _.cloneDeep(myProfile);
+    profileStorage.setMyProfile(myProfile);
   }
 };
